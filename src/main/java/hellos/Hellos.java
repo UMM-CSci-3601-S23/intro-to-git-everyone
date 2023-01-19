16558853b108f3931079bb86bbf42243cbd634c7
Brendan Conroy <conro188@morris.umn.edu>
2023-01-19 13:56:19 -0600

    Added our methods

--- src/main/java/hellos/Hellos.java
+++ src/main/java/hellos/Hellos.java
@@ -18,64 +18,19 @@ public class Hellos {
 
   public String generateOutput() {
     StringBuilder builder = new StringBuilder();
-    builder.append(alexSaysHello());
-    builder.append(alwinSaysHello());
-    builder.append(alwinSaysHelloAgain());
-    builder.append(ashSaysHello());
-    builder.append(ashSaysHelloAgain());
-    builder.append(gregSaysHello());
-    builder.append(harrySaysHello());
-    builder.append(jaydonSaysHello());
-    builder.append(kkSaysHello());
-    builder.append(nicSaysHello());
-    builder.append(noahSaysHello());
-    builder.append(willSaysHello());
+
+    builder.append(brendanSaysHello());
+    builder.append(masonSaysHello());
+
     return builder.toString();
   }
 
 
-  private static String nicSaysHello() {
-    return "Nic says 'Howdy!'\n";
-  }
-  private static String willSaysHello() {
-    return "Will says 'Howdy!'\n";
-  }
-  private static String harrySaysHello() {
-    return "Harry says 'Howdy!'\n";
-  }
-  private static String kkSaysHello() {
-   return "KK says 'Hello!'\n";
+  private static String brendanSaysHello() {
+    return "Brendan says 'Howdy!'\n";
   }
 
-  private static String alwinSaysHello() {
-    return "Alwin says 'How are ya!'\n";
-  }
-
-  private static String ashSaysHello() {
-    return "Ash says 'Hello!'\n";
-  }
-
-  private String alwinSaysHelloAgain() {
-    return "Alwin says 'How are ya again!'\n";
-  }
-
-  private String ashSaysHelloAgain() {
-    return "Ash says 'Hello again!'\n";
-  }
-
-  private String noahSaysHello() {
-    return "Noah says 'Hey!'\n";
-  }
-
-  private String jaydonSaysHello() {
-    return "Jaydon says 'yoooooooooooooooooooooooooooooo!'\n";
-  }
-
-  private String alexSaysHello() {
-    return "Alex says 'Hello!'\n";
-  }
-
-  private String gregSaysHello() {
-    return "Greg says 'top of the Murnin!'\n";
+  private static String masonSaysHello() {
+    return "Mason says 'Howdy!'\n";
   }
 }
